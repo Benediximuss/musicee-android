49c8c691ce3634af2af608d21c252269a9ec6964
Benediximuss <ugurpasa699@gmail.com>
2023-11-16 02:52:17 +0300

    "initial commit"

---
+++ android/app/src/main/kotlin/com/example/musicee_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.musicee_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/SignUpPage.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+
+MaterialColor customPrimarySwatch = const MaterialColor(0xFFE98E8D, {
+  50: Color(0xFFFFF5F5),
+  100: Color(0xFFFFE0E0),
+  200: Color(0xFFFCB9B8),
+  300: Color(0xFFFA8F8E),
+  400: Color(0xFFF96B6A),
+  500: Color(0xFFE98E8D),
+  600: Color(0xFFD17E7D),
+  700: Color(0xFFB36B6A),
+  800: Color(0xFF944A49),
+  900: Color(0xFF7A3635),
+});
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  // Controllers for form fields
+  final _usernameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  // Focus nodes for form fields
+  final _usernameFocus = FocusNode();
+  final _emailFocus = FocusNode();
+  final _passwordFocus = FocusNode();
+
+  // Validation function
+  bool _validateInputs() {
+    if (_usernameController.text.isEmpty) {
+      setState(() {
+        _usernameFocus.requestFocus();
+      });
+      return false;
+    } else if (_emailController.text.isEmpty) {
+      setState(() {
+        _emailFocus.requestFocus();
+      });
+      return false;
+    } else if (_passwordController.text.isEmpty) {
+      setState(() {
+        _passwordFocus.requestFocus();
+      });
+      return false;
+    }
+    return true;
+  }
+
+  @override
+  void dispose() {
+    // Dispose controllers and focus nodes to avoid memory leaks
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _usernameFocus.dispose();
+    _emailFocus.dispose();
+    _passwordFocus.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: customPrimarySwatch,
+      ),
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Create Your Account',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: customPrimarySwatch.shade700,
+                  ),
+                ),
+                const SizedBox(height: 64),
+                TextFormField(
+                  controller: _usernameController,
+                  focusNode: _usernameFocus,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    icon: Icon(Icons.person),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: _usernameController.text.isEmpty
+                            ? Colors.red
+                            : customPrimarySwatch.shade700,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _emailController,
+                  focusNode: _emailFocus,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    icon: Icon(Icons.email),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: _emailController.text.isEmpty
+                            ? Colors.red
+                            : customPrimarySwatch.shade700,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _passwordController,
+                  focusNode: _passwordFocus,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    icon: Icon(Icons.lock),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: _passwordController.text.isEmpty
+                            ? Colors.red
+                            : customPrimarySwatch.shade700,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 64),
+                SizedBox(
+                  width: 400,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Validate inputs before submitting
+                      if (_validateInputs()) {
+                        // sign-up logic
+                        Navigator.pop(context);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                    child: const Text(
+                      'Create account',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 48),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
